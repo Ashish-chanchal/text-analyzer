8d4d140d9461a7372eb64929efed105ca6522b43
Ashish Chanchal <akchanchal2002@gmail.com>
2023-02-01 05:15:36 +0000

    add comments

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
+// input textarea
 let input1 = document.getElementById("textinput1");
+// output textarea
 let input2 = document.getElementById("textinput2");
+// upper case
 let upper = document.getElementById("up-case");
+// lower case
 let lower = document.getElementById("low-case");
+// remove space
 let space = document.getElementById("sp-case");
+// remove symbols
 let punctution = document.getElementById("pu-case");
+// copy to clipbord
 let copytxt = document.getElementById("cp-case");
 
+// eventlistner for upper case
 upper.addEventListener('click', funk1);
+// event listner for lowe case
 lower.addEventListener('click', funk2);
+// remove extra spaces
 space.addEventListener('click', funk3);
+// remove punctuation
 punctution.addEventListener('click',funk4);
+// copy text
 copytxt.addEventListener('click',funk5);
+
+// eventlistner for upper case
 function funk1(){
   if(input1.value!=""){
     let txt=input1.value;
@@ -18,6 +32,8 @@ function funk1(){
     input1.value="";
   }
 }
+
+// event listner for lowe case
 function funk2(){
   if(input1.value!=""){
     let txt=input1.value;
@@ -25,6 +41,8 @@ function funk2(){
     input1.value="";
   }
 }
+
+// remove extra spaces
 function funk3(){
   if(input1.value!=""){
     let txt=input1.value;
@@ -33,6 +51,8 @@ function funk3(){
     input1.value="";
   }
 }
+
+// remove punctuation
 function funk4(){
   if(input1.value!=""){
     let txt=input1.value;
@@ -40,6 +60,8 @@ function funk4(){
     input1.value="";
   }
 }
+
+// copy text
 function funk5(){
  if (input2.value!="") {
   let copyText=document.getElementById("textinput2");
